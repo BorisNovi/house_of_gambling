4ad244d750c0dc7dbc88add5032455a14f7e360f
Boris Novi <borisnovikov4815@gmail.com>
2023-10-12 19:24:45 +0400

    feat: add banner and styles for header

--- house_of_gambling/src/app/components/header/header.component.ts
+++ house_of_gambling/src/app/components/header/header.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  lang: boolean = false;
 
+  toggleLang() {
+    this.lang = !this.lang;
+  }
 }
